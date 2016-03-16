a140ec48e527cf1378224bd449a705d6604e8a83
Markham Shofner <markham.shofner@gmail.com>
2016-03-16 12:01:51 -0400

    continue euler 9, set up initial functions

--- 9/9.js
+++ 9/9.js
@@ -8,9 +8,21 @@ console.log("hello");
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
 
-var a, b, c;
-
 // triplet - is a < b < c
+function isTriplet (a, b, c) {
+  if (a < b && b < c) {
+    return true;
+  } else {
+    return false;
+  }
+}
 // pythagorean triplet?
+
 // sums to 1000?
 // product
+
+//actual call function
+function mainCall (ax, bx, cx) {
+  isTriplet (ax, bx, cx);
+
+}
